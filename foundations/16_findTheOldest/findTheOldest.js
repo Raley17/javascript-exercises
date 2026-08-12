2ed82812ead47b9b7a3b496d8a05ed414a83530d
Rama <ramirologiurato@gmail.comgit config --global user.name Ramagit statusgit config --global user.email ramirologiurato@gmail.com2>
2026-08-11 23:09:03 -0300

    Finished

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,6 +1,12 @@
-const findTheOldest = function() {
+const findTheOldest = (arr) =>
+  arr.reduce((lastGuy, nextGuy) => {
+    const lastGuyDeath = lastGuy.yearOfDeath || new Date().getFullYear();
+    const nextGuyDeath = nextGuy.yearOfDeath || new Date().getFullYear();
 
-};
+    const edadLastGuy = lastGuyDeath - lastGuy.yearOfBirth;
+    const edadNextGuy = nextGuyDeath - nextGuy.yearOfBirth;
+    return edadLastGuy > edadNextGuy ? lastGuy : nextGuy;
+  });
 
 // Do not edit below this line
 module.exports = findTheOldest;
