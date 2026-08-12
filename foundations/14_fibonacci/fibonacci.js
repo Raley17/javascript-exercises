a5f2d685382b8eaed960c4faaca32e27262a8e0c
Rama <ramirologiurato@gmail.comgit config --global user.name Ramagit statusgit config --global user.email ramirologiurato@gmail.com2>
2026-08-11 22:15:42 -0300

    Finished.

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,6 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function (num) {
+  let a = 0,
+    b = 1;
+  for (let i = 0; i < num; i++) {
+    let temp = a;
+    a = b;
+    b = temp + b;
+  }
+  return a;
 };
+
+console.log(fibonacci(4));
 
 // Do not edit below this line
 module.exports = fibonacci;
